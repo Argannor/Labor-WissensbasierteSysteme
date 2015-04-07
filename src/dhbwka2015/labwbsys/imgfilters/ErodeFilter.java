b05bfd2a41375a1261b969bb9987801655b7be08
Th3Tw0 <thetwo@gmx.net>
2015-04-07 16:43:14 +0200

    added some comments

--- src/dhbwka2015/labwbsys/imgfilters/ErodeFilter.java
+++ src/dhbwka2015/labwbsys/imgfilters/ErodeFilter.java
@@ -37,6 +37,12 @@ public class ErodeFilter implements ImageFilterIf {
         process(outModel, outRaster, result);
     }
 
+    /**
+     * Processes the result and therefore generates the output image
+     * @param outModel
+     * @param outRaster
+     * @param result
+     */
     private void process(ColorModel outModel, WritableRaster outRaster, boolean[][] result) {
         for (int i = 0; i < width; i++) {
             for (int j = 0; j < height; j++) {
@@ -45,12 +51,26 @@ public class ErodeFilter implements ImageFilterIf {
         }
     }
 
+    /**
+     * Calculate the output color for a given boolean value
+     * @param b
+     * @return
+     */
     private int calcColor(boolean b) {
         if(b)
             return 0xffffffff;
         return 0xff000000;
     }
 
+    /**
+     * Calculates whether the pixel at (i|j) will be 1 (true) or 0 (false) using the following erosion matrix:
+     *     |010|
+     * M = |111|
+     *     |010|
+     * @param i component of the pixels position
+     * @param j component of the pixels position
+     * @return result of erosion
+     */
     private boolean erode(int i, int j) {
         if(i < 1 || j < 1 || i >= width - 1 || j >= height - 1) {
             return false;
@@ -58,17 +78,23 @@ public class ErodeFilter implements ImageFilterIf {
         return isTrue(i, j) && isTrue(i - 1, j) && isTrue(i+1, j) && isTrue(i, j-1) && isTrue(i, j+1);
     }
 
+    /**
+     * Calculates the binary value of a given pixel based on it's brightness
+     * @param i component of the pixels position
+     * @param j component of the pixels position
+     * @return binary value
+     */
     private boolean isTrue(int i, int j) {
         return calcBrightnessValue(inputRaster.getDataElements(i, j, null)) > 127;
     }
 
     /**
      * Calculates the Brightness of a given pixel
-     * @param pix
-     * @return
+     * @param pixel
+     * @return brightness
      */
-    int calcBrightnessValue(Object pix) {
-        int rgb = inputModel.getRGB(pix);
+    int calcBrightnessValue(Object pixel) {
+        int rgb = inputModel.getRGB(pixel);
 
         int r = (rgb & 0x00ff0000) >> 16;
         int g = (rgb & 0x0000ff00) >> 8;
